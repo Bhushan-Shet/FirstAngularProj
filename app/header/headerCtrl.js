5bfa2dfa72e57d895ac305b9fdc81347bf12296f
Bhushan-Shet <bhushanshet@yahoo.co.in>
2017-01-25 13:46:21 -0800

    25 th january

--- app/header/headerCtrl.js
+++ app/header/headerCtrl.js
@@ -1,10 +1,21 @@
 (function(){
     
-    function headerCtrl(){
+    function headerCtrlfn(){
       var vm=this;
         vm.brandName="Good Services";
-        vm.navItems =["Login","Register","Cart"];
+        vm.navItems =["Login","Register","Cart","LogOut"];
+        vm.loginTemplate="app/login/login.tpl.html";
+        vm.registerTemplate="app/register/register.tpl.html";
+        vm.loadView= function(param){
+            console.log(param);
+            if (param=="Login"){
+                vm.loadTemplate=vm.loginTemplate;
+            }
+            else if (param=="Register"){
+                vm.loadTemplate=vm.registerTemplate;              
+            }
+        };
     }
     angular.module("header")
-    .controller("headerCtrl",[headerCtrl]);
+    .controller("headerCtrl",[headerCtrlfn]);
 })();
--- app/register/registerCtrl.js
+++ app/register/registerCtrl.js
@@ -1,7 +1,14 @@
 (function(){
     
     function registerCtrl(){
-        
+        var vm=this;
+        vm.user={
+            firstName:""   
+        };
+        vm.register=function(){
+            
+            console.log(vm.user);
+        }
     }
     angular.module("register")
         .controller("registerCtrl",[registerCtrl])
