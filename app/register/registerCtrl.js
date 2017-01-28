087c148fbe0adca287088e6225c469d7e549fbe5
Bhushan-Shet <bhushanshet@yahoo.co.in>
2017-01-28 15:34:45 -0800

    Updated till 27th january

--- app/register/registerCtrl.js
+++ app/register/registerCtrl.js
@@ -1,6 +1,6 @@
 (function(){
     
-    function registerCtrl(){
+    function registerCtrl(lookupSvc){
         var vm=this;
         vm.user={
             firstName:""   
@@ -9,8 +9,31 @@
             
             console.log(vm.user);
         }
+        //vm.countries= lookupSvc.getCountries(); 
+        
+        lookupSvc.getCountries()
+        .then(function(response){
+            console.log(response);
+            
+        })
+        .catch(function(errorResponce){
+            console.log(errorResponce);
+             
+        })
+        
+        vm.selectedCountry=lookupSvc.defaultCountry();
+        
+        lookupSvc.getCountriesFromJson()
+        .then(function(res){
+            console.log(res);
+            vm.countries=res;
+        })
+        .catch(function(err){
+            console.log(err);
+             
+        })
     }
     angular.module("register")
-        .controller("registerCtrl",[registerCtrl])
+        .controller("registerCtrl",["lookupService",registerCtrl])
     
 })();
